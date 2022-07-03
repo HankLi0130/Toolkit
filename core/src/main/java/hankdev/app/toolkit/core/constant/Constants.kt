941a7675046468cdb8da5804f027de82d0803fa6
hankli <HankLi0130@gmail.com>
2022-07-03 09:23:13 +0800

    Add core module

---
+++ core/src/main/java/hankdev/app/toolkit/core/constant/Constants.kt
@@ -0,0 +1,8 @@
+package hankdev.app.toolkit.core.constant
+
+const val EMPTY = ""
+const val ZERO = 0
+const val ZERO_LONG = 0L
+const val ZERO_FLOAT = 0.0F
+const val ZERO_DOUBLE = 0.0
+const val NO_RESOURCE = ZERO
