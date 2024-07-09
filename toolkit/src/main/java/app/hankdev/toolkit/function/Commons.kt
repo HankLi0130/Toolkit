ab26b01de7eec6400ce0fe9e247c4b23138a29d0
Hank Li <HankLi0130@gmail.com>
2024-07-09 09:02:02 +0800

    Update Commons

--- toolkit/src/main/java/app/hankdev/toolkit/function/Commons.kt
+++ toolkit/src/main/java/app/hankdev/toolkit/function/Commons.kt
@@ -1,6 +1,7 @@
 package app.hankdev.toolkit.function
 
 import android.content.Context
+import androidx.annotation.StyleRes
 import androidx.appcompat.app.AlertDialog
 import app.hankdev.toolkit.NO_RESOURCE
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
@@ -21,6 +22,16 @@ fun showMaterialAlertDialog(
     setBuilder: MaterialAlertDialogBuilder.() -> Unit
 ): AlertDialog {
     val builder = MaterialAlertDialogBuilder(context, overrideThemeResId)
+    builder.setBuilder()
+    return builder.show()
+}
+
+fun showAlertDialog(
+    context: Context,
+    @StyleRes themeResId: Int = NO_RESOURCE,
+    setBuilder: AlertDialog.Builder.() -> Unit
+): AlertDialog {
+    val builder = AlertDialog.Builder(context, themeResId)
     builder.setBuilder()
     return builder.show()
 }
